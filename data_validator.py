6f1f4a82f80786cc0d620ec94026d5455d92c99a
fredriklo <fredrik.n.lokke@gmail.com>
2023-12-08 00:18:02 +0100

    Third commit
    This commit uses a more code golf-like solution, turning the entire method into a one-liner. While the approach is the same as in the previous commit, I have now used a list comprehension instead of a for-loop.

--- data_validator.py
+++ data_validator.py
@@ -1,11 +1,6 @@
 class DataValidator:
     
     def check_positive_int(list_of_strings):
-        valid_positive_ints = []
-        for string in list_of_strings:
-            if string.isnumeric():
-                valid_positive_ints.append(string)
-        return valid_positive_ints
-    
+        return [num for num in list_of_strings if num.isnumeric()]
     
 print(DataValidator.check_positive_int(["123", "asd", "-12", "popo", "90", "1", "0"]))
